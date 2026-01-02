fc2fd99fbc15407af70568442bca2fbd78f76fbb
Nimps2 <86239766+Nimps2@users.noreply.github.com>
2026-01-02 13:18:36 +0000

    Ex4 Completed

--- module02/ex4/ft_raise_errors.py
+++ module02/ex4/ft_raise_errors.py
@@ -1,16 +1,58 @@
-class PlantErrors(Exception):
-    pass
+def check_plant_health(
+        plant_name: str, water_level: int, sunlight_hours: int
+        ) -> str:
+    if not plant_name:
+        raise ValueError("Plant name cannont be empty!\n")
 
-class PlantNameError(PlantErrors):
-    pass
+    if water_level < 1:
+        raise ValueError(f"Water level {water_level} is too low (min 1)\n")
+    if water_level > 10:
+        raise ValueError(f"Water level {water_level} is too high (max 10)\n")
 
-class WaterLevelError(PlantErrors):
-    pass
+    if sunlight_hours < 2:
+        raise ValueError(
+            f"Sunlight hours {sunlight_hours} is too low (min 2)\n"
+            )
+    if sunlight_hours > 12:
+        raise ValueError(
+            f"Sunlight hours {sunlight_hours} is too high (max 12)\n"
+            )
 
-class SunlightLevelError(PlantErrors):
-    pass
+    return (f"Plant '{plant_name}' is healthy!\n")
 
-def check_plant_health(plant_name, water_level, sunlight_hours):
-try:
-    if plant_name == "":
-    
+
+def test_plant_checks():
+    print("=== Garden Plant Health Checker ===\n")
+
+    print("Testing good values...")
+    try:
+        print(check_plant_health("Tomato", 4, 5))
+    except ValueError as error:
+        print(f"Error: {error}")
+
+    print("Testing empty plant name...")
+    try:
+        print(check_plant_health(None, 4, 5))
+    except ValueError as error:
+        print(f"Error: {error}")
+
+    print("Testing bad water level...")
+    try:
+        print(check_plant_health("Tomato", 15, 5))
+    except ValueError as error:
+        print(f"Error: {error}")
+
+    print("Testing bad sunlight hours...")
+    try:
+        print(check_plant_health("Tomato", 4, 0))
+    except ValueError as error:
+        print(f"Error: {error}")
+    print("All error raising testes completed!", end="")
+
+
+def main():
+    test_plant_checks()
+
+
+if __name__ == "__main__":
+    main()
