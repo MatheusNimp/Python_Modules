cb9ded22a2fdc4a26c50c2e2c678a58663ce857a
MatheusNimp <matheusnimp1@gmail.com>
2026-01-05 22:03:36 +0000

    ex2_module03

--- module03/ex2/ft_coordinate_system.py
+++ module03/ex2/ft_coordinate_system.py
@@ -3,18 +3,13 @@ import math
 
 
 def parser(coordinates: str):
-    try:
-        parts = coordinates.split(",")
-        if len(parts) != 3:
-            raise ValueError("Expected exactly 3 coordinates")
-    except ValueError as error:
-        print(f"Error parsing coordinates: {error}")
-    try:
-        x = int(parts[0])
-        y = int(parts[1])
-        z = int(parts[2])
-    except ValueError as error:
-        print(f"Error parsing coordinates: {error}")
+    parts = coordinates.split(",")
+    if len(parts) != 3:
+        raise ValueError("Expected exactly 3 coordinates")
+
+    x = int(parts[0])
+    y = int(parts[1])
+    z = int(parts[2])
     return (x, y, z)
 
 
@@ -26,29 +21,55 @@ def calculate_distance(pos1, pos2):
 
 
 def main():
-    print("=== Game Coordinate System ===")
+    print("=== Game Coordinate System ===\n")
 
     spawn = (0, 0, 0)
     pos1 = (10, 20, 5)
-    print(f"Position Created: {pos1}")
-
+    print(f"Position created: {pos1}")
     distance1 = calculate_distance(spawn, pos1)
-    print(f"Distance betwewn {spawn} and {pos1}: {distance1}")
+    print(f"Distance between {spawn} and {pos1}: {distance1:.2f}\n")
+
     if len(sys.argv[1:]) == 1:
         coordinates = sys.argv[1]
         print(f'Parsing coordinates: "{coordinates}"')
+        try:
+            pos2 = parser(coordinates)
+            print(f"Parsed position: {pos2}")
+            distance2 = calculate_distance(spawn, pos2)
+            print(f"Distance between {spawn} and {pos2}: {distance2:.2f}")
 
+            print("\n Unpacking Demonstration:")
+            x, y, z = pos2
+            print(f"Player at x={x}, y={y}, z={z}")
+            print(f"Coordinates: X={x}, Y={y}, Z={z}\n")
+
+        except ValueError as error:
+            print(f"Error parsing coordinates: {error}")
+            print(f"Error details- Type: ValueError, Args: {error.args}")
+
+    print('Parsing coordinates: "3,4,0"')
+    pos_out = parser("3,4,0")
+    print(f"Parsed position: {pos_out}")
+    print(
+        f"Distance between {spawn} and {pos_out}:"
+        f" {calculate_distance(spawn, pos_out)}"
+        )
+
+    print('\nParsing invalid coordinates: "abc,def,ghi"')
     try:
-        pos2 = parser(coordinates)
-        print(f"Parsed position: {pos2}")
-
-        distance2 = calculate_distance(spawn, pos2)
-        print(f"Distance betwewn {spawn} and {pos2}: {distance2}")
-
-        print("\n Unpacking Demonstration:")
-        x, y, z = pos2
-        print(f"Player at x={x}, y={y}, z={z}")
-        print(f"Coordinates: X={x}, Y={y}, Z={z}")
-
+        parser("abc,def,ghi")
     except ValueError as error:
         print(f"Error parsing coordinates: {error}")
+        print(
+            f"Error details- Type: ValueError,"
+            f" Args: {error.args}"
+            )
+
+    print("\nUnpacking demonstration:")
+    x, y, z = pos_out
+    print(f"Player at x={x}, y={y}, z={z}")
+    print(f"Coordinates: X={x}, Y={y}, Z={z}", end="")
+
+
+if __name__ == "__main__":
+    main()
